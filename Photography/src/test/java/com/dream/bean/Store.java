aac7a8d796dee7fbedb269747ce585ddf231352d
wangsonggenHomePC <wangsonggenHomePC@dream.com>
2014-09-21 19:51:27 +0800

    初始化摄影协会项目

---
+++ Photography/src/test/java/com/dream/bean/Store.java
@@ -0,0 +1,44 @@
+package com.dream.bean;
+
+public class Store {
+	private String storeId;
+
+	private String storeName;
+
+	private String storeDetail;
+
+	private String areaId;
+
+	public String getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(String storeId) {
+		this.storeId = storeId;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+
+	public String getStoreDetail() {
+		return storeDetail;
+	}
+
+	public void setStoreDetail(String storeDetail) {
+		this.storeDetail = storeDetail;
+	}
+
+	public String getAreaId() {
+		return areaId;
+	}
+
+	public void setAreaId(String areaId) {
+		this.areaId = areaId;
+	}
+
+}
---
+++ Photography/src/test/java/com/dream/bean/User.java
@@ -0,0 +1,65 @@
+package com.dream.bean;
+
+public class User {
+
+	private String username;
+
+	private String password;
+
+	private String passwd;
+
+	private String type;
+
+	private String telephone;
+
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
