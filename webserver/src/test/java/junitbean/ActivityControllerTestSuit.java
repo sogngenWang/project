d8efdd3ac28d472866206bf56c431219c74d59a9
wsg <wsg@126.com>
2014-10-31 11:53:38 +0800

    使用注解方式来做JUnit

--- webserver/src/test/java/junitbean/ActivityControllerTestSuit.java
+++ webserver/src/test/java/junitbean/ActivityControllerTestSuit.java
@@ -14,8 +14,10 @@ import com.dream.service.ActivityService;
  * 
  */
 public class ActivityControllerTestSuit extends JUnitControllerBase {
+	
 	@Autowired
 	ActivityService service;
+	
 
 	@Test
 	public void addActivityController() throws Exception {
@@ -23,6 +25,7 @@ public class ActivityControllerTestSuit extends JUnitControllerBase {
 		request.addParameter("request", "{\"content\":{\"username\":\"5CFFFFFFF238FFFFFFB67547FFFFFFC223\"},\"head\":{\"brand\":\"HTC\",\"imei\":\"356871046099762\",\"imsi\":\"1234567890ABCDEF\",\"model\":\"Evo 3D GSM\",\"netype\":\"NETWORK_TYPE_WIFI\",\"platform\":\"Android\",\"release\":\"4.2.2\",\"sdk\":\"17\",\"serial\":\"HT22PV203276\",\"time\":\"2014-10-24 13:51:55,611\",\"versionCode\":\"1\",\"versionName\":\"1.0\"},\"mac\":\"F4F26357424C5C61\"}");
 		this.excuteAction(request, response);
 	}
+	
 	
 	@Test
 	public void detailActivityController() throws Exception {
@@ -34,10 +37,14 @@ public class ActivityControllerTestSuit extends JUnitControllerBase {
 	}
 	
 	@Test
+	@RequestServletPath( "/listActivity" )
+	@RequestParameter(name="request",value="{\"content\":{},\"head\":{\"brand\":\"HTC\",\"imei\":\"356871046099762\",\"imsi\":\"1234567890ABCDEF\",\"model\":\"Evo 3D GSM\",\"netype\":\"NETWORK_TYPE_WIFI\",\"platform\":\"Android\",\"release\":\"4.2.2\",\"sdk\":\"17\",\"serial\":\"HT22PV203276\",\"time\":\"2014-10-24 13:51:55,611\",\"versionCode\":\"1\",\"versionName\":\"1.0\"},\"mac\":\"F4F26357424C5C61\"}")
 	public void listActivityController() throws Exception {
-		request.setServletPath("/listActivity");
-		request.addParameter("request", "{\"content\":{},\"head\":{\"brand\":\"HTC\",\"imei\":\"356871046099762\",\"imsi\":\"1234567890ABCDEF\",\"model\":\"Evo 3D GSM\",\"netype\":\"NETWORK_TYPE_WIFI\",\"platform\":\"Android\",\"release\":\"4.2.2\",\"sdk\":\"17\",\"serial\":\"HT22PV203276\",\"time\":\"2014-10-24 13:51:55,611\",\"versionCode\":\"1\",\"versionName\":\"1.0\"},\"mac\":\"F4F26357424C5C61\"}");
-		this.excuteAction(request, response);
+//		request.setServletPath("/listActivity");
+//		request.addParameter("request", "{\"content\":{},\"head\":{\"brand\":\"HTC\",\"imei\":\"356871046099762\",\"imsi\":\"1234567890ABCDEF\",\"model\":\"Evo 3D GSM\",\"netype\":\"NETWORK_TYPE_WIFI\",\"platform\":\"Android\",\"release\":\"4.2.2\",\"sdk\":\"17\",\"serial\":\"HT22PV203276\",\"time\":\"2014-10-24 13:51:55,611\",\"versionCode\":\"1\",\"versionName\":\"1.0\"},\"mac\":\"F4F26357424C5C61\"}");
+//		this.excuteAction(request, response);
 	}
+	
+	
 	
 }
