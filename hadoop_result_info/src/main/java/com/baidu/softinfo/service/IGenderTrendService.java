d85ce7808a495ea07e8eac7fc0ba95eb4844a262
wsg <wsg@126.com>
2015-04-20 13:11:16 +0800

    123

--- hadoop_result_info/src/main/java/com/baidu/softinfo/service/IGenderTrendService.java
+++
@@ -1,9 +0,0 @@
-package com.baidu.softinfo.service;
-
-import com.baidu.base.model.ResultObj;
-
-public interface IGenderTrendService {
-
-	ResultObj getGenderTrend(String software);
-
-}
--- hadoop_result_info/src/main/java/com/baidu/softinfo/service/IPashionService.java
+++
@@ -1,9 +0,0 @@
-package com.baidu.softinfo.service;
-
-import com.baidu.base.model.ResultObj;
-
-public interface IPashionService {
-
-	ResultObj getPashion(String software);
-
-}
